45816ab663735f7769c0b865a9d54f8cfac1cbad
Brandon <Brandon Sissenah>
2020-01-30 09:41:56 -0500

    Dice working, no style taking effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//created dice variable and a random number generator between 1-6 for each chance of the dice landing
+
 let dice = {
     chance: 6,
     activate: function() {
@@ -10,8 +12,6 @@ function getNumber(output) {
     var box = document.getElementById('box');
     box.innerHTML = output;
 }
-
-var Btn = document.getElementById('Btn');
 
 Btn.onclick = function() {
     var result = dice.activate();
